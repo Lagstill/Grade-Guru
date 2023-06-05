30fbed75819e7b88757a0b5c17c16a37b008c0e8
Lagstill <kpdlakshmi1970@gmail.com>
2023-06-05 17:31:36 +0530

    Unit Test Added

---
+++ test_app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+import pytest
+
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello, World!' in response.data
